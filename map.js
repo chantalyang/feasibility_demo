33fa3593e7075181faa35dd0471704dd67f30a66
Chantal <chantalyang17@gmail.com>
2015-07-19 17:47:08 +0200

    Set div height to display map

---
+++ map.js
@@ -0,0 +1,18 @@
+var map = AmCharts.makeChart( "map_div", {
+	type: "map",
+  "theme": "light",
+
+  dataProvider: {
+  	map: "worldLow",
+
+
+
+
+
+  },
+
+  areasSettings: {
+    unlistedAreasColor: "#FFCC00"
+  }
+
+} );
